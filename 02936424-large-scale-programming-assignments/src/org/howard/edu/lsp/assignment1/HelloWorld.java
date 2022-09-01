0d0e674b910e8b637ca9820d70822dcae63175c4
AyomideCode <thebeep41@gmail.com>
2022-09-01 19:28:08 -0400

    Initial commit for HW #1

---
+++ 02936424-large-scale-programming-assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * @author Ayomide Okeshola @02936424
+ *
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+	}
+}
