1b93957bb6512cc4cb33ac1a69029dddd9833091
AyomideCode <thebeep41@gmail.com>
2022-10-27 23:51:18 -0400

    Starting JUnit 5 testing

---
+++ 02936424-large-scale-programming-assignments/src/org/howard/edu/lsp/midterm/problem1/VotingMachineTest.java
@@ -0,0 +1,19 @@
+package org.howard.edu.lsp.midterm.problem1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VotingMachineTest {
+
+	@Test
+	void testGetVotes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSum() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
